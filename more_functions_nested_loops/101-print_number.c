488b00f0c34ab153b96038470f65849139b069e1
diegosmen111 <dbav_10@hotmail.com>
2022-10-09 22:21:24 -0700

    fix print number

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * main.
- *
- * Return: Always 0.
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
  */
-
 void print_number(int n)
 {
 	unsigned int m, d, count;
