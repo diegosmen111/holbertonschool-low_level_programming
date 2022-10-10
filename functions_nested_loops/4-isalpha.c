1e2d09264268c55b0112770dc40b70b6b1c71657
diegosmen111 <dbav_10@hotmail.com>
2022-10-09 21:28:14 -0700

    Isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha - Shiws 1 of the imput is lowercase or uppercase
+ * Another cases, shows 0
+ *
+ * @c: The charactee in ASCII code
+ *
+ * Return: 1 for letters. Returns 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
