322126a62fad872aec5b83d1b31668600415a120
diegosmen111 <dbav_10@hotmail.com>
2023-03-07 22:59:13 -0800

    fixe update

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * main.h
- *
- *
- * Return: The number of nodes in h.
- */
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -28,4 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif /* main_h */
+
+#endif
